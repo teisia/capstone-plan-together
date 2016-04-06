1a05780b704f43228948deb2ce018536562dc7d2
Teisia Park <teisia@gmail.com>
2016-04-06 00:50:17 -0600

    i think google sign in is working

--- app.js
+++ app.js
@@ -5,11 +5,34 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 require('dotenv').load();
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth2').Strategy;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: '746466032586-fkn4lk9v4pccpa005accokik9u2m13cb.apps.googleusercontent.com',
+    clientSecret: 'n52Pq21dadcVTiD44doP7eBE',
+    callbackURL: 'http://localhost:3000/auth/google/callback',
+    passReqToCallback   : true
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+       process.nextTick(function () {
+         return done(null, profile);
+    });
+  }
+));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,6 +45,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+ res.render('index', { user: req.user });
 });
+
+router.get('/auth/google',
+  passport.authenticate('google', { scope:
+  	[ 'https://www.googleapis.com/auth/plus.login',
+  	  'https://www.googleapis.com/auth/calendar',
+      'https://www.googleapis.com/auth/gmail.compose' ] }
+));
+
+router.get('/auth/google/callback',
+	passport.authenticate( 'google', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+}));
+
+router.get('/logout', function(req, res, next) {
+  req.logout();
+  res.redirect('/');
+})
+
 
 module.exports = router;
